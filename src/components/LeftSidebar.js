544fc4ebc1a886a77b23faf10a495aee6df24d1d
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-12-11 15:49:19 +0100

    init

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import pic from '../../public/pic.png'
 
 function LeftSidebar() {
   return (
-    <div className="App">
+    <div className="navbar">
+        <Link />
+        <div className='navs'>
+
+        </div>
+        <div className='navs'>
+
+        </div>
     </div>
   );
 }
