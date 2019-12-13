ef6c090c0bf69ee7143b456cbb47597214ddea6c
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-12-13 15:57:39 +0100

    init

--- src/sections/RightSidebar.js
+++ src/sections/RightSidebar.js
@@ -5,26 +5,26 @@ import React from 'react';
 const RightSidebar = () => {
   
   return (
-    <div style={rBar}>
-      <div style={tTop} >
-        <div style={title}>
+    <div className='r-bar'>
+      <div className='r-top' >
+        <div className='title'>
           New Order
         </div>
-        <div style={order}>
+        <div className='order'>
           Fried Rice, Big Chicken, Salad, Big Eva Bottle Water
         </div>
-        <div style={sold}>
+        <div className='sold'>
           <p>Total Amount</p>
           <p >N23,560</p>
-          <button style={button}>CONFIRM</button>
+          <button className='button'>CONFIRM</button>
         </div>
       </div>
-      <div style={tTop} >
-        <div style={title}>
+      <div className='r-top' >
+        <div className='title'>
           Add item to list
           <input placeholder='Filter List' />
         </div>
-        <div style={order}>
+        <div className='order'>
           <h4>Meat</h4>
           <label for='meat-'>
             <input type='checkbox' name='meat-' />Big goat meat
@@ -69,46 +69,46 @@ const RightSidebar = () => {
 
 export default RightSidebar;
 
-const rBar = {
-  width: "500px",
-  borderLeft: "2px solid #9391B1",
-  padding: "15px",
-  boxSizing: "border-box",
-  color: '#72748E',
-  // fontSize: '1rem',
-};
-const tTop = {  
-  height: "252px",
-  width: "100%",
-  borderRadius: "5px",
-  border: '1px solid #F0F0F7',
-};
-const title = {
-  width: "100%",
-  background: "#F0F0F7",
-  padding: '12px 20px',
-  textAlign: 'right',
-  display: 'inline-flex'
-};
-const order = {
-  width: '100%',
-  height: '153px',
-  borderBottom: '1px solid #F0F0F7',
-  padding: '10px 28px',
-}
-const sold = {
-  width: '100%',
-  height: '53px',
-  display: 'inline-flex',
-  alignItems: 'center',
-  padding: '0 20px',
-  justifyContent: 'space-between'
-}
-const button = {
-  padding: '7px 23px',
-  background: '#221ECC',
-  borderRadius: '20px',
-  border: 0,
-  color: '#fff',
-  textTransform: 'uppercase'
-}
+// const rBar = {
+//   width: "500px",
+//   borderLeft: "2px solid #9391B1",
+//   padding: "15px",
+//   boxSizing: "border-box",
+//   color: '#72748E',
+//   // fontSize: '1rem',
+// };
+// const tTop = {  
+//   height: "252px",
+//   width: "100%",
+//   borderRadius: "5px",
+//   border: '1px solid #F0F0F7',
+// };
+// const title = {
+//   width: "100%",
+//   background: "#F0F0F7",
+//   padding: '12px 20px',
+//   textAlign: 'right',
+//   display: 'inline-flex'
+// };
+// const order = {
+//   width: '100%',
+//   height: '153px',
+//   borderBottom: '1px solid #F0F0F7',
+//   padding: '10px 28px',
+// }
+// const sold = {
+//   width: '100%',
+//   height: '53px',
+//   display: 'inline-flex',
+//   alignItems: 'center',
+//   padding: '0 20px',
+//   justifyContent: 'space-between'
+// }
+// const button = {
+//   padding: '7px 23px',
+//   background: '#221ECC',
+//   borderRadius: '20px',
+//   border: 0,
+//   color: '#fff',
+//   textTransform: 'uppercase'
+// }
