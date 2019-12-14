5d91aa5b70f218c8999e88575fb23c8a562e26e9
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-12-14 14:39:47 +0100

    init

--- src/sections/LeftSidebar.js
+++ src/sections/LeftSidebar.js
@@ -6,22 +6,22 @@ function LeftSidebar() {
   return (
     <div className="l-bar">
         <div className='navs'>
-          <img src={pic} alt='menu' width='45px' height='45px' />
+          <div className='circle'>a</div>
           <Link>
-            <img src={pic} alt='menu' width='45px' height='45px' />
-            Meadle
+            <img src={pic} alt='menu' width='24px' height='24px' />
+            <p>Sale</p>
           </Link>
           <Link>
-            <img src={pic} alt='menu' width='45px' height='45px' />
-            Meadle
+            <img src={pic} alt='menu' width='24px' height='24px' />
+            <p>Stock</p>
           </Link>
           <Link>
-            <img src={pic} alt='menu' width='45px' height='45px' />
-            Meadle
+            <img src={pic} alt='menu' width='24px' height='24px' />
+            <p>Price</p>
           </Link>
         </div>
         <div className='navs'>
-          <img src={pic} alt='menu' width='45px' height='45px' />
+          <div className='circle'>a</div>
         </div>
     </div>
   );
